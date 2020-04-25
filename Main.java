b86cbd05a1097771b1a9bb029687667c58ad93ad
Saumitra Kumar Paul <skpaul@gmail.com>
2020-04-25 14:05:46 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,35 +12,109 @@ public class Main
 	    
 		try {
 			Scanner scan = new Scanner(System.in);
-			System.out.println("Enter the number of places");
-			Integer  numberOfDistricts = scan.nextInt();
-			District[] districts= new District[numberOfDistricts];
-			Integer counter = 0;
-			do {
+
+			List<District> districtList  = new ArrayList<>();
+			districtList = enterDistrict(districtList, scan);
+			
+			// System.out.println("Enter the number of places");
+			// Integer  numberOfDistricts = scan.nextInt();
+			// District[] districts= new District[numberOfDistricts];
+			// Integer counter = 0;
+			// do {
 				
-				System.out.println("Enter the name of district ...");
-				String distName = scan.next();
+			// 	System.out.println("Enter the name of district ...");
+			// 	String distName = scan.next();
 				 
-				if(isExist(distName, districts)){
-					System.out.println("This place is already exists ...");
-				}
-				else{
-					districts[counter] = new District("" + counter, distName );
-					counter++;
-				}
+			// 	if(isExist(distName, districts)){
+			// 		System.out.println("This place is already exists ...");
+			// 	}
+			// 	else{
+			// 		districts[counter] = new District("" + counter, distName );
+			// 		counter++;
+			// 	}
 				
-			} while (counter < numberOfDistricts);
+			// } while (counter < numberOfDistricts);
 			   
 	
-			 
-			 System.out.println("Enter the number of roads");
-			 Integer  numberOfRoads = scan.nextInt();
-			 Road[] roads = new Road[numberOfRoads];
-			 counter = 0;
+			List<Road> roadList  = new ArrayList<>();
+			roadList = enterRoad(roadList, scan);
+			//  System.out.println("Enter the number of roads");
+			//  Integer  numberOfRoads = scan.nextInt();
+			//  Road[] roads = new Road[numberOfRoads];
+			//  counter = 0;
 			
-			do{
-				System.out.println("-----------------------------------------------------");
+			// do{
+			// 	System.out.println("-----------------------------------------------------");
 				
+			// 	System.out.println("Road#"+ counter +" - Enter details ...");
+			// 	System.out.println("    District of origin ...");
+			// 	String origin = scan.next();
+
+			// 	if(!isExist(origin, districts)){
+			// 	   System.out.println("This district does not exist");
+			// 	   continue;
+			// 	}
+		   
+			// 	District originDistrict = find(origin, districts);
+
+			// 	System.out.println("    District of destination ...");
+			// 	String  destination = scan.next();
+
+			// 	if(!isExist(destination, districts)){
+			// 		System.out.println("This district does not exist");
+			// 		continue;
+			// 	 }
+			
+			// 	 District destinationDistrict = find(destination, districts);
+
+			// 	System.out.println("    Distnance ...");
+			// 	Integer  distnance = scan.nextInt();
+
+			// 	Road newRoad = new Road(""+ counter, originDistrict, destinationDistrict, distnance);
+			// 	roads[counter] = newRoad;
+			// 	counter++;
+			//  }while(counter < numberOfRoads);
+			 
+
+			// List<District> districtList = Arrays.asList(districts); 
+			
+			Route route = new Route(districtList, Arrays.asList(roads));
+			
+			findRoute(route, districtList, scan);
+			
+			System.out.println("Good Bye");
+
+			scan.close();
+		 
+		 } catch(Exception e) {
+			 System.out.println(e.getMessage());
+		 }
+	}
+	
+	static List<District> enterDistrict(List<District> existingList, Scanner scan){
+		System.out.println("Enter the name of district ... [enter 'n' for no]");
+		String name = scan.next();
+		if(!name.equals("no")){
+			if(isExist(name, existingList)){
+				System.out.println("This place is already exists ...");
+				enterDistrict(existingList, scan);
+			}
+			else{
+				Integer count = existingList.size();
+				District dist = new District("" + count, name);
+				existingList.add(dist);
+				enterDistrict(existingList, scan);
+			}
+		}
+
+		return existingList;
+	}
+	
+	static List<Road> enterRoad(List<Road> existingRoads, List<District> districts, Scanner scan){
+		System.out.println("Enter the name of district ... [enter 'n' for no]");
+		String name = scan.next();
+		Integer counter = existingRoads.size() + 1;
+		if(!name.equals("no")){
 				System.out.println("Road#"+ counter +" - Enter details ...");
 				System.out.println("    District of origin ...");
 				String origin = scan.next();
@@ -65,36 +140,16 @@ public class Main
 				Integer  distnance = scan.nextInt();
 
 				Road newRoad = new Road(""+ counter, originDistrict, destinationDistrict, distnance);
-				roads[counter] = newRoad;
-				counter++;
-			 }while(counter < numberOfRoads);
-			 
+				existingRoads.add(newRoad);
+				
+		}
 
-			List<District> districtList = Arrays.asList(districts); 
-			
-			Route route = new Route(districtList, Arrays.asList(roads));
-			
-			findRoute(route, districtList, scan);
-			
-			System.out.println("Good Bye");
+		return existingRoads;
+	}
 
-			scan.close();
-		 
-		 } catch(Exception e) {
-			 System.out.println(e.getMessage());
-		 }
-	}
-	
-	static List<District> enterDistrict(List<District> existingList, Scanner scan){
-		System.out.println("Where do you want to start your journey? ...");
-		String name = scan.next();
-		District dist = new District("id", name);
-		return existingList;
-	}
-	
-	static boolean isExist(String name, District[] districts ){
-		
-		if(districts.length == 0){
+	static boolean isExist(String name, List<District> districts ){
+
+		if(districts.size() == 0){
 			return false;
 		}
 		for (District dist : districts) {
@@ -105,12 +160,30 @@ public class Main
 				}
 			}
 			
-        }
+		}
 		//throw new RuntimeException("Should not happen");
 		return false;
 	}
+
+	// static boolean isExist(String name, District[] districts ){
+		
+	// 	if(districts.length == 0){
+	// 		return false;
+	// 	}
+	// 	for (District dist : districts) {
+	// 		if(dist != null){
+	// 			String nn = dist.getName();
+	// 			if (nn.equals(name)) {
+	// 				return true;
+	// 			}
+	// 		}
+			
+	// 	}
+	// 	//throw new RuntimeException("Should not happen");
+	// 	return false;
+	// }
 	
-	static District find(String name, District[] districts ){
+	static District find(String name, List<District> districts ){
 		for (District dist : districts) {
             String nn = dist.getName();
 				if (nn.equals(name)) {
@@ -120,6 +193,17 @@ public class Main
 		throw new RuntimeException("Should not happen");
 		
 	}
+
+	// static District find(String name, District[] districts ){
+	// 	for (District dist : districts) {
+    //         String nn = dist.getName();
+	// 			if (nn.equals(name)) {
+	// 				return dist;
+	// 			}
+    //     }
+	// 	throw new RuntimeException("Should not happen");
+		
+	// }
 
 	static Integer getIndex(String name, List<District>districtList ){
 		Integer index = 0;
